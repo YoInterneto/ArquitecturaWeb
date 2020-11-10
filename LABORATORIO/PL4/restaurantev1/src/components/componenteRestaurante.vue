fb2b30d129d27d765315052b3e3f253fb49b66f4
Alberto González Martínez <albergonmar@gmail.com>
2020-11-10 13:41:03 +0100

    refactorizacion componentes

--- LABORATORIO/PL4/restaurantev1/src/components/componenteRestaurante.vue
+++ LABORATORIO/PL4/restaurantev1/src/components/componenteRestaurante.vue
@@ -7,7 +7,7 @@
             <div class="choose-table">
                 <select id="table" v-model="tableElection">
                     <option :value="option.id" v-for="(option, index) in tableOptions" :key="index">
-                        {{ option.nombre }}
+                        {{ option.name }}
                     </option>
                 </select>
             </div>
@@ -16,65 +16,43 @@
             <!-- - - - - - - - - - - - - -  -->
             <div class="choose-dish" v-for="(option, index) in dishOptions" :key="index">
                 <input type="radio" name="dish" :value="option.id" v-model="typeElection">
-                {{option.nombre}}
+                {{option.name}}
             </div>
         </div>
         <div class="dishes-panel">
             <!-- - - - - - - - - - - - - - - - - - - - - - - - - -->
-            <!-- TODAS LAS OPCIONES DE ELECCION DE UN TIPO PLATO -->
+            <!-- MUESTRA LAS OPCIONES DE ELECCION DE UN TIPO PLATO Y DA OPCION DE ELEGIR-->
             <!-- - - - - - - - - - - - - - - - - - - - - - - - - -->
             <div class="option-panel">
-                <select id="options1" v-model="dishElection" v-if="typeElection == 'primero'" @dblclick="addDish(dishElection, typeElection)" multiple>
-                    <option :value="option.id" v-for="(option, index) in firstOptions" :key="index">
-                        {{ option.nombre }}  ({{ option.precio}}€)
-                    </option>
-                </select>
-                <select id="options2" v-model="dishElection" v-else-if="typeElection == 'segundo'" @dblclick="addDish(dishElection, typeElection)" multiple>
-                    <option :value="option.id" v-for="(option, index) in secondOptions" :key="index">
-                        {{ option.nombre }}  ({{ option.precio}}€)
-                    </option>
-                </select>
-                <select id="options3" v-model="dishElection" v-else @dblclick="addDish(dishElection, typeElection)" multiple>
-                    <option :value="option.id" v-for="(option, index) in dessertOptions" :key="index">
-                        {{ option.nombre }}  ({{ option.precio}}€)
-                    </option>
-                </select>
+                <optionDishes v-if="typeElection == 'primero'"
+                    :dishes = this.firstOptions
+                    @add="addDish"
+                />
+                <optionDishes v-else-if="typeElection == 'segundo'"
+                    :dishes = this.secondOptions
+                    @add="addDish"
+                />
+                <optionDishes v-else
+                    :dishes = this.dessertOptions
+                    @add="addDish"
+                />
             </div>
             <!-- - - - - - - - - - - - - - - - - - - -  -->
-            <!-- TODAS LOS PLATOS ELEGIDOS POR UNA MESA -->
+            <!-- MUESTRA LOS PLATOS ELEGIDOS POR UNA MESA -->
             <!-- - - - - - - - - - - - - - - - - - - -  -->
             <div class="selected-panel">
-                <select id="selected1" v-if="tableElection == 'mesa1'" multiple aria-readonly="true">
-                    <option :value="option.id" v-for="(option, index) in dishesM1" :key="index">
-                        {{ option.nombre }}
-                    </option>
-                </select>
-                <select id="selected1" v-else-if="tableElection == 'mesa2'" multiple aria-readonly="true">
-                    <option :value="option.id" v-for="(option, index) in dishesM2" :key="index">
-                        {{ option.nombre }}
-                    </option>
-                </select>
-                <select id="selected1" v-else-if="tableElection == 'mesa3'" multiple aria-readonly="true">
-                    <option :value="option.id" v-for="(option, index) in dishesM3" :key="index">
-                        {{ option.nombre }}
-                    </option>
-                </select>
-                <select id="selected1" v-else-if="tableElection == 'mesa4'" multiple aria-readonly="true">
-                    <option :value="option.id" v-for="(option, index) in dishesM4" :key="index">
-                        {{ option.nombre }}
-                    </option>
-                </select>
-                <select id="selected1" v-else multiple aria-readonly="true">
-                    <option :value="option.id" v-for="(option, index) in dishesM5" :key="index">
-                        {{ option.nombre }}
-                    </option>
-                </select>
+                <selectedDishes v-if="this.tableElection == 'mesa1'" :dishes = this.dishesM1></selectedDishes>
+                <selectedDishes v-else-if="this.tableElection == 'mesa2'" :dishes = this.dishesM2></selectedDishes>
+                <selectedDishes v-else-if="this.tableElection == 'mesa3'" :dishes = this.dishesM3></selectedDishes>
+                <selectedDishes v-else-if="this.tableElection == 'mesa4'" :dishes = this.dishesM4></selectedDishes>
+                <selectedDishes v-else :dishes = this.dishesM5></selectedDishes>
             </div>
         </div>
         <div class="options-panel">
             <!-- - - - - - - - - - - - - - - - -  -->
             <!-- OPCIONES DE ELEGIR CAFE Y/O COPA -->
             <!-- - - - - - - - - - - - - - - - -  -->
+            <!-- Se puede poner de que cada mesa tenga su propia opción de checkbox  -->
             <div class="options-checkbox">
                 <div class="coffe-option">
                     <input type="checkbox" id="coffe" v-model="coffeElection">
@@ -108,11 +86,15 @@
 </template>
 
 <script>
+import selectedDishes from '@/components/selectedDishes'
+import optionDishes from '@/components/optionDishes'
+
 export default {
     name: "componenteRestaurante",
 
     components: {
-
+        selectedDishes,
+        optionDishes
     },
 
     data(){
@@ -125,7 +107,7 @@ export default {
             coffeElection: false,
             cocktailElection: false,
 
-            //Precio de la cuenta de la mesa
+            //price de la cuenta de la mesa
             totalM1: 0,
             totalM2: 0,
             totalM3: 0,
@@ -143,95 +125,85 @@ export default {
             //SI NO FUNCIONA PONER UN ID QUE SEA EL INDEX DE CADA UNO Y SACAR LOS PLATOS A PARTIR DE AHI
             //BUSCAR OTRA OPCION MEJOR, ¿SE PUEDE PASAR DIRECTAMENTE EL OBJETO ELEGIDO EN UN <SELECT></SELECT>?
             dishOptions: [
-                {id: 'primero', nombre: 'Primeros platos'},
-                {id: 'segundo', nombre: 'Segundos platos'},
-                {id: 'postre', nombre: 'Postres'}
+                {id: 'primero', name: 'Primeros platos'},
+                {id: 'segundo', name: 'Segundos platos'},
+                {id: 'postre', name: 'Postres'}
             ],
             tableOptions: [
-                {id: 'mesa1', nombre: 'Mesa 1'},
-                {id: 'mesa2', nombre: 'Mesa 2'},
-                {id: 'mesa3', nombre: 'Mesa 3'},
-                {id: 'mesa4', nombre: 'Mesa 4'},
-                {id: 'mesa5', nombre: 'Mesa 5'}
+                {id: 'mesa1', name: 'Mesa 1'},
+                {id: 'mesa2', name: 'Mesa 2'},
+                {id: 'mesa3', name: 'Mesa 3'},
+                {id: 'mesa4', name: 'Mesa 4'},
+                {id: 'mesa5', name: 'Mesa 5'}
             ],
             firstOptions: [
-                {id: 1, nombre: "Sopa de fideos", precio: 4},
-                {id: 2, nombre: "Ensalada de la casa", precio: 3},
-                {id: 3, nombre: "Macarrones bolognesa", precio: 5},
-                {id: 4, nombre: "Pure de verduras", precio: 2},
-                {id: 5, nombre: "Entremeses", precio: 5},
+                {id: 1, name: "Sopa de fideos", price: 4},
+                {id: 2, name: "Ensalada de la casa", price: 3},
+                {id: 3, name: "Macarrones bolognesa", price: 5},
+                {id: 4, name: "Pure de verduras", price: 2},
+                {id: 5, name: "Entremeses", price: 5},
             ],
             secondOptions: [
-                {id: 1, nombre: "Solomillo", precio: 10},
-                {id: 2, nombre: "Bacalao con patatas", precio: 7},
-                {id: 3, nombre: "Hamburguesa completa", precio: 7},
-                {id: 4, nombre: "Pizza 4 quesos", precio: 8},
-                {id: 5, nombre: "Costillas barbacoa", precio: 13},
-                {id: 6, nombre: "Sandwich vegetal", precio: 6},
-                {id: 7, nombre: "Parillada de verduras", precio: 8},
+                {id: 1, name: "Solomillo", price: 10},
+                {id: 2, name: "Bacalao con patatas", price: 7},
+                {id: 3, name: "Hamburguesa completa", price: 7},
+                {id: 4, name: "Pizza 4 quesos", price: 8},
+                {id: 5, name: "Costillas barbacoa", price: 13},
+                {id: 6, name: "Sandwich vegetal", price: 6},
+                {id: 7, name: "Parillada de verduras", price: 8},
             ],
             dessertOptions: [
-                {id: 1, nombre: "Arroz con leche", precio: 3},
-                {id: 2, nombre: "Flan", precio: 2},
-                {id: 3, nombre: "Fruta de temporada", precio: 2},
-                {id: 4, nombre: "Natillas", precio: 4},
+                {id: 1, name: "Arroz con leche", price: 3},
+                {id: 2, name: "Flan", price: 2},
+                {id: 3, name: "Fruta de temporada", price: 2},
+                {id: 4, name: "Natillas", price: 4},
             ]
         }
     },
 
     methods: {
         showDishes(dish){
-            console.log(`Has seleccionado ${dish.nombre} - ${dish.id}`);
+            console.log(`Has seleccionado ${dish.name} - ${dish.id}`);
         },
         showTable(id){
             console.log(`Has seleccionado ${id}`);
         },
-        addDish(id, type){
-            var dish = {};
-
-            if (type == 'primero') {
-                dish = this.firstOptions[id-1];
-            }else if(type == 'segundo') {
-                dish = this.secondOptions[id-1];
-            }else {
-                dish = this.dessertOptions[id-1];
-            }
-
+        addDish(dishSelected){
             switch (this.tableElection) {
                 case 'mesa1':
+                    this.totalM1 += dishSelected.price;
                     this.dishesM1.unshift({
-                        id: dish.id,
-                        nombre: dish.nombre,
-                    })
-                    this.totalM1 += dish.precio;
+                        id: dishSelected.id,
+                        name: dishSelected.name,
+                    });
                     break;
                 case 'mesa2':
+                    this.totalM2 += dishSelected.price;
                     this.dishesM2.unshift({
-                        id: dish.id,
-                        nombre: dish.nombre,
-                    })
-                    this.totalM2 += dish.precio;
+                        id: dishSelected.id,
+                        name: dishSelected.name,
+                    });
                     break;
                 case 'mesa3':
+                    this.totalM3 += dishSelected.price;
                     this.dishesM3.unshift({
-                        id: dish.id,
-                        nombre: dish.nombre,
-                    })
-                    this.totalM3 += dish.precio;
+                        id: dishSelected.id,
+                        name: dishSelected.name,
+                    });
                     break;
                 case 'mesa4':
+                    this.totalM4 += dishSelected.price;
                     this.dishesM4.unshift({
-                        id: dish.id,
-                        nombre: dish.nombre,
-                    })
-                    this.totalM4 += dish.precio;
+                        id: dishSelected.id,
+                        name: dishSelected.name,
+                    });
                     break;
                 case 'mesa5':
+                    this.totalM5 += dishSelected.price;
                     this.dishesM5.unshift({
-                        id: dish.id,
-                        nombre: dish.nombre,
-                    })
-                    this.totalM5 += dish.precio;
+                        id: dishSelected.id,
+                        name: dishSelected.name,
+                    });
                     break;
                 default:
                     console.log(`ERROR: Número de mesa incorrecto - valor ${this.tableElection}`);
---
+++ LABORATORIO/PL4/restaurantev1/src/components/optionDishes.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main-div">
+    <select id="options" v-model="dishElection" @dblclick="addDish(dishElection, dishes)" multiple>
+      <option :value="option.id" v-for="(option, index) in dishes" :key="index">
+        {{ option.name }}  ({{ option.price}}€)
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  name: 'optionDishes',
+
+  props: {
+    dishes: {
+      type: Array,
+      required: true,
+    }
+  },
+
+  data() {
+    return {
+      dishElection: [],
+    }
+  },
+
+  methods: {
+    addDish(position, dishArray){
+      var dish = dishArray[position-1];
+      console.log(`Se ha seleccionado ${dish.id}) ${dish.name}`);
+      this.$emit('add', dish);
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
